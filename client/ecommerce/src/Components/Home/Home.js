2250791b165558cb7151abf48a3e2180b4b82c3a
vallecbg <valioplayofficial@gmail.com>
2020-07-12 20:06:12 +0300

    Feat: Implemented TextSection Component for Home Page

--- client/ecommerce/src/Components/Home/Home.js
+++ client/ecommerce/src/Components/Home/Home.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import {
-  Grid,
-  createStyles,
-  Container,
-  Button
-} from '@material-ui/core/';
-import {
   makeStyles
 } from '@material-ui/core/styles'
 import Hero from './Hero';
+import TextSection from './TextSection'
 
 const useStyles = makeStyles((theme) => ({
   content: {},
@@ -25,15 +20,27 @@ const heroImages = [
 const Home = () => {
   const classes = useStyles()
   return (
-    <Hero
-      backgroundImg={heroImages[Math.floor(Math.random() * heroImages.length)]}
-      titleText="REACTIFY SHOP"
-      subtitleText={
-        <span>50% SALE TO ALL LAPTOPS &#8226; 25% TO ALL SMARTPHONES</span>
-      }
-      primaryBtnText="SIGN IN"
-      primaryBtnLink="/register"
-    />
+    <div>
+      <Hero
+        backgroundImg={heroImages[Math.floor(Math.random() * heroImages.length)]}
+        titleText="REACTIFY SHOP"
+        subtitleText={
+          <span>50% SALE TO ALL LAPTOPS &#8226; 25% TO ALL SMARTPHONES</span>
+        }
+        primaryBtnText="SIGN IN"
+        primaryBtnLink="/register"
+      />
+      <TextSection
+        text="Our mission as an Reactify Shop is to provide the customer the best experience and huge promotions while shopping."
+        bgColor="#3F51B5"
+        textColor="#fff"
+        borderColor="#998643"
+        padding="12px 0"
+        btnLink="/about/vision-charisms"
+        btnText="Check our promos"
+        darkBg={true}
+      />
+    </div>
   )
 }
 
---
+++ client/ecommerce/src/Components/Home/TextSection.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import { Button } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+
+const styles = theme => ({
+  section: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    padding: '38px 0',
+    textAlign: 'center'
+  },
+  innerContainer: {
+    width: '70%',
+    margin: '0 auto'
+  },
+  sectionText: {
+    fontSize: '28px',
+    fontWeight: 400,
+    lineHeight: 1.5
+  },
+  heroBtn: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '16px'
+  },
+  invertedBtn: {
+    color: '#3F51B5',
+    backgroundColor: 'transparent',
+    border: '2px #3F51B5 solid',
+    boxShadow: 'none'
+  },
+  invertedBtnDark: {
+    color: '#fff',
+    backgroundColor: 'transparent',
+    border: '2px #fff solid',
+    boxShadow: 'none'
+  },
+  white: {
+    color: '#fff'
+  },
+  navLink : {
+    textDecoration: 'none',
+    color : 'white'
+  }
+})
+
+class TextSection extends Component {
+  render () {
+    const {
+      classes,
+      sectionTitle,
+      text,
+      btnLink,
+      btnText,
+      textColor,
+      bgColor,
+      borderColor,
+      darkBg,
+      padding
+    } = this.props
+
+    const ButtonComponent = (
+      <Grid item className={classes.heroBtn} xs={12}>
+        <Link className={classes.navLink} to={btnLink}>
+            {/* TODO: Change the button link to all products page or smth else */}
+            <Button
+            title={btnText}
+            className={darkBg ? classes.invertedBtnDark : classes.invertedBtn}
+            color="secondary"
+            style={{ width: '192px' }}
+            >
+            {btnText}
+            </Button>
+        </Link>
+      </Grid>
+    )
+
+    return (
+      <div
+        className={classes.section}
+        style={{
+          backgroundColor: bgColor,
+          borderTop: `2px solid ${borderColor}`,
+          borderBottom: `2px solid ${borderColor}`,
+          padding: padding
+        }}
+      >
+        <div className={classes.innerContainer}>
+          <Typography
+            variant="display2"
+            gutterBottom
+            style={{ color: textColor }}
+          >
+            {sectionTitle}
+          </Typography>
+
+          <Typography
+            className={classes.sectionText}
+            variant="body1"
+            gutterBottom
+            style={{ color: textColor }}
+          >
+            {text}
+          </Typography>
+          {btnLink && btnText ? ButtonComponent : null}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(TextSection)
