893165c6197fa7aae9cb1d2b1cb077a517913690
vallecbg <valioplayofficial@gmail.com>
2020-07-14 01:57:27 +0300

    Feat: Created User Service to send HTTP Requests to server using axios

---
+++ client/ecommerce/src/Services/userService.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:8000/api/auth'
+
+const userService = {
+    login: function (data) {
+        return axios.post(`${baseUrl}/login`, data, {
+                withCredentials: true
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    },
+    register: function (data) {
+        return axios.post(`${baseUrl}/register`, data, {
+            withCredentials: true
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    },
+    // logout: function() {
+    //     return axios.get(`${baseUrl}/logout`, {
+    //         withCredentials: true
+    //     })
+    //     .catch((error) => {
+    //         console.error(error)
+    //     })
+    // }
+}
+
+export default userService
