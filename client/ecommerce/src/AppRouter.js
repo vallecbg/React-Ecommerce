5ba56e70e713fd141cc79d253844647cc6600480
vallecbg <valioplayofficial@gmail.com>
2020-07-16 23:52:44 +0300

    Feat: Added protection to the routes

--- client/ecommerce/src/AppRouter.js
+++ client/ecommerce/src/AppRouter.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Home from './Components/Home/Home'
 import SignUp from './Components/Authentication/Register/SignUp'
 import SignIn from './Components/Authentication/Login/SignIn'
-
+import { StoreContext } from './Store/Store'
 
 const AppRouter = () => {
+    const { state } = useContext(StoreContext)
+    const AuthRoute = ({ path, component}) => {
+        return state.isAuth ? <Redirect to={'/'} /> : <Route path={path} component={component} />
+    }
+    // const ProtectedRoute = ({ path, component }) => {
+    //     return state.isAuth ? <Route path={path} component={component} /> : <Redirect to={'/'} />
+    // }
+
     return (
         <Switch>
             <Route path="/" exact component={Home} />
-            <Route path="/register" component={SignUp} />
-            <Route path="/login" component={SignIn} />
+            <AuthRoute path="/register" component={SignUp} />
+            <AuthRoute path="/login" component={SignIn} />
         </Switch>
     )
 }
