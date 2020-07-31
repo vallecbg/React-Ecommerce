079b5784c014369dbe4d11fdb738eb3edfe44b49
vallecbg <valioplayofficial@gmail.com>
2020-08-01 00:10:57 +0300

    Feat: Implemented increase and decrease quantity

--- client/ecommerce/src/Components/Cart/Cart.js
+++ client/ecommerce/src/Components/Cart/Cart.js
@@ -1,6 +1,13 @@
 import React, { useContext } from "react";
-//import { StoreContext } from '../../Store/Store'
+import { StoreContext } from "../../Store/Store";
+import {
+  removeProductFromCartSuccess,
+  updateCartSuccess,
+} from "../../Store/Actions";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import IndeterminateCheckBoxIcon from "@material-ui/icons/IndeterminateCheckBox";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import {
   Container,
   Box,
@@ -13,6 +20,7 @@ import {
   IconButton,
   Avatar,
   Typography,
+  Button,
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 
@@ -45,8 +53,30 @@ const useStyles = makeStyles((theme) =>
 );
 
 const Cart = () => {
-  //const { state, dispatch } = useContext(StoreContext)
+  const { state, dispatch } = useContext(StoreContext);
   const classes = useStyles();
+
+  const removeProduct = (product) => {
+    dispatch(removeProductFromCartSuccess(product));
+  };
+
+  const increaseQuantity = (product) => {
+    dispatch(
+      updateCartSuccess({
+        product,
+        value: product.quantity + 1,
+      })
+    );
+  };
+
+  const decreaseQuantity = (product) => {
+    dispatch(
+      updateCartSuccess({
+        product,
+        value: product.quantity <= 1 ? 1 : product.quantity - 1,
+      })
+    );
+  };
 
   const cart = JSON.parse(window.localStorage.getItem("cart"));
   const renderProducts = (cart || []).map((product) => {
@@ -72,16 +102,40 @@ const Cart = () => {
                 >
                   &#36;{(product.price / 100).toFixed(2)}
                 </Typography>
-                {` — ${product.description}`}
+                {` — ${product.description}`} <br />
+                <Typography
+                  component="span"
+                  variant="body1"
+                  className={classes.inline}
+                  color="error"
+                >
+                  {`Quantity : ${product.quantity}`}
+                </Typography>
               </React.Fragment>
             }
           />
+
           <ListItemSecondaryAction>
+            <IconButton
+              color="primary"
+              edge="end"
+              aria-label="increase"
+              onClick={() => increaseQuantity(product)}
+            >
+              <AddCircleIcon />
+            </IconButton>
+            <IconButton
+              color="secondary"
+              edge="end"
+              aria-label="decrease"
+              onClick={() => decreaseQuantity(product)}
+            >
+              <IndeterminateCheckBoxIcon />
+            </IconButton>
             <IconButton
               edge="end"
               aria-label="delete"
-              //TODO: remove item
-              //onClick={}
+              onClick={() => removeProduct(product)}
             >
               <DeleteIcon />
             </IconButton>
@@ -93,7 +147,10 @@ const Cart = () => {
   });
 
   const renderTotalPrice =
-    (cart || []).reduce((acc, current) => (acc += current.price), 0) / 100;
+    (cart || []).reduce(
+      (acc, current) => (acc += current.price * current.quantity),
+      0
+    ) / 100;
 
   return (
     <Container maxWidth="md" className={classes.underHeader}>
@@ -106,12 +163,20 @@ const Cart = () => {
         </Typography>
         <List className={classes.root}>
           {renderProducts.length ? renderProducts : null}
-          <ListItem className={classes.listItem}>
+          <ListItem>
             <Typography variant="subtitle1" className={classes.total}>
-              Total: &#36;{renderProducts.length ? (
-                  renderTotalPrice
-              ): 0.00}
+              Total: &#36;{renderProducts.length ? renderTotalPrice : 0.0}
             </Typography>
+          </ListItem>
+          <ListItem className={classes.listItem}>
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={renderProducts.length <= 0}
+            >
+              <ShoppingCartIcon />
+              Checkout
+            </Button>
           </ListItem>
         </List>
       </Box>
