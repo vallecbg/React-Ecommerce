8247591bceab9ca8ad54724e9fa5aa1f874c2bda
vallecbg <valioplayofficial@gmail.com>
2020-08-10 17:27:20 +0300

    Fix: Removed products array in category model because it's not needed

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose');
-const { String, ObjectId } = mongoose.Schema.Types;
+const { String } = mongoose.Schema.Types;
 
 const categorySchema = new mongoose.Schema({
     title: {
         type: String,
         required: true
-    },
-    products: [{
-        type: ObjectId,
-        ref: "Product"
-    }]
+    }
 })
 
 module.exports = mongoose.model('Category', categorySchema);
