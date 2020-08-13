c1f0e2e7c7653e0e386002f8b3225b9d849a30f0
vallecbg <valioplayofficial@gmail.com>
2020-08-13 20:29:52 +0300

    Fix: App Bar on logged in user not showing button for dashboard

--- client/ecommerce/src/Components/AppBar/AppBarAuth.js
+++ client/ecommerce/src/Components/AppBar/AppBarAuth.js
@@ -1,154 +1,211 @@
-import React, { Component, useContext } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component, useContext } from "react";
+import PropTypes from "prop-types";
 import {
-  AppBar, Toolbar, Typography, List, ListItem,
-  withStyles, Grid, SwipeableDrawer, Button, Badge
-} from '@material-ui/core';
-import { Link } from 'react-router-dom'
-import MenuIcon from '@material-ui/icons/Menu';
-import HomeIcon from '@material-ui/icons/Home'
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
-import AccountCircleIcon from '@material-ui/icons/AccountCircle'
-import { logout } from '../../Store/Actions'
-import { StoreContext } from '../../Store/Store'
-import CartButton from '../Cart/CartButton'
-import DashboardIcon from '@material-ui/icons/Dashboard'
+  AppBar,
+  Toolbar,
+  Typography,
+  List,
+  ListItem,
+  withStyles,
+  Grid,
+  SwipeableDrawer,
+  Button,
+  Badge,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+import MenuIcon from "@material-ui/icons/Menu";
+import HomeIcon from "@material-ui/icons/Home";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import { logout } from "../../Store/Actions";
+import { StoreContext } from "../../Store/Store";
+import CartButton from "../Cart/CartButton";
+import DashboardIcon from "@material-ui/icons/Dashboard";
 
-class AppBarAuth extends Component{
-
-  constructor(props){
+class AppBarAuth extends Component {
+  constructor(props) {
     super(props);
-    this.state = {drawerActivate:false, drawer:false};
+    this.state = { drawerActivate: false, drawer: false };
     this.createDrawer = this.createDrawer.bind(this);
     this.destroyDrawer = this.destroyDrawer.bind(this);
   }
 
-  componentDidMount(){
-    if(window.innerWidth <= 600){
-      this.setState({drawerActivate:true});
+  componentDidMount() {
+    if (window.innerWidth <= 600) {
+      this.setState({ drawerActivate: true });
     }
 
-    window.addEventListener('resize',()=>{
-      if(window.innerWidth <= 600){
-        this.setState({drawerActivate:true});
-      }
-      else{
-        this.setState({drawerActivate:false})
+    window.addEventListener("resize", () => {
+      if (window.innerWidth <= 600) {
+        this.setState({ drawerActivate: true });
+      } else {
+        this.setState({ drawerActivate: false });
       }
     });
   }
 
   //Small Screens
-  createDrawer(){
-    const {classes, history, dispatch, roleName, cartLength} = this.props
-    
+  createDrawer() {
+    const { classes, history, dispatch, roleName, cartLength } = this.props;
+
     const logoutFunction = () => {
-      dispatch(logout())
-      history.push('/')
-    }
-    
+      dispatch(logout());
+      history.push("/");
+    };
+
     return (
       <div>
-        <AppBar >
+        <AppBar>
           <Toolbar>
-            <Grid container direction = "row" justify = "space-between" alignItems="center">
+            <Grid
+              container
+              direction="row"
+              justify="space-between"
+              alignItems="center"
+            >
               <MenuIcon
-                className = {this.props.classes.sideBarIcon}
-                onClick={()=>{this.setState({drawer:true})}} />
+                className={this.props.classes.sideBarIcon}
+                onClick={() => {
+                  this.setState({ drawer: true });
+                }}
+              />
 
-              <Typography color="inherit" variant = "h6">Reactify</Typography>
-              <Typography color="inherit" variant = "h1">
+              <Typography color="inherit" variant="h6">
+                Reactify
+              </Typography>
+              <Typography color="inherit" variant="h1">
                 <CartButton cartLength={cartLength} />
               </Typography>
-              
             </Grid>
           </Toolbar>
         </AppBar>
 
         <SwipeableDrawer
-         open={this.state.drawer}
-         onClose={()=>{this.setState({drawer:false})}}
-         onOpen={()=>{this.setState({drawer:true})}}>
-           <ListItem key={1} divider>
-            <Typography color="inherit" variant = "h6">Reactify</Typography>
-           </ListItem>
-           <div
-             tabIndex={0}
-             role="button"
-             onClick={()=>{this.setState({drawer:false})}}
-             onKeyDown={()=>{this.setState({drawer:false})}}>
-            <List className = {this.props.classes.list}>
-                <Link className={classes.navLinkMobile} to='/'>
-                  <ListItem key = {2} button divider> 
-                    <HomeIcon className={classes.menuIcon}/>
-                    Home 
+          open={this.state.drawer}
+          onClose={() => {
+            this.setState({ drawer: false });
+          }}
+          onOpen={() => {
+            this.setState({ drawer: true });
+          }}
+        >
+          <ListItem key={1} divider>
+            <Typography color="inherit" variant="h6">
+              Reactify
+            </Typography>
+          </ListItem>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              this.setState({ drawer: false });
+            }}
+            onKeyDown={() => {
+              this.setState({ drawer: false });
+            }}
+          >
+            <List className={this.props.classes.list}>
+              <Link className={classes.navLinkMobile} to="/">
+                <ListItem key={2} button divider>
+                  <HomeIcon className={classes.menuIcon} />
+                  Home
+                </ListItem>
+              </Link>
+              <Link className={classes.navLinkMobile} to="/products">
+                <ListItem key={3} button divider>
+                  <ShoppingBasketIcon className={classes.menuIcon} />
+                  All Products
+                </ListItem>
+              </Link>
+              {roleName === "Admin" ? (
+                <Link className={classes.navLinkMobile} to="/dashboard">
+                  <ListItem key={4} button divider>
+                    <DashboardIcon className={classes.menuIcon} />
+                    Admin Panel
                   </ListItem>
                 </Link>
-                <Link className={classes.navLinkMobile} to='/products'>
-                  <ListItem key = {3} button divider> 
-                    <ShoppingBasketIcon className={classes.menuIcon}/>
-                    All Products 
+              ) : (
+                <Link className={classes.navLinkMobile} to="/myOrders">
+                  <ListItem key={4} button divider>
+                    <DashboardIcon className={classes.menuIcon} />
+                      Dashboard
                   </ListItem>
                 </Link>
-               {roleName === 'Admin' ? (
-                 <Link className={classes.navLinkMobile} to='/dashboard'>
-                    <ListItem key = {4} button divider> 
-                      <DashboardIcon className = {classes.menuIcon}/>  
-                      Admin Panel 
-                    </ListItem>
-                  </Link>
-               ) : null}
-               
-               <ListItem key = {5} button divider onClick={logoutFunction}> 
-                <AccountCircleIcon className = {classes.menuIcon}/>  
-                Logout 
-               </ListItem>
-             </List>
+              )}
 
-         </div>
-       </SwipeableDrawer>
-
+              <ListItem key={5} button divider onClick={logoutFunction}>
+                <AccountCircleIcon className={classes.menuIcon} />
+                Logout
+              </ListItem>
+            </List>
+          </div>
+        </SwipeableDrawer>
       </div>
     );
   }
 
   //Larger Screens
-  destroyDrawer(){
-    const {classes, history, dispatch, roleName, cartLength} = this.props
+  destroyDrawer() {
+    const { classes, history, dispatch, roleName, cartLength } = this.props;
 
     const logoutFunction = () => {
-        dispatch(logout())
-        history.push('/')
-    }
+      dispatch(logout());
+      history.push("/");
+    };
 
     return (
       <AppBar>
         <Toolbar>
-          <Typography variant = "h6" style={{flexGrow:1}} color="inherit" ><Link className={classes.navLink} to="/">Reactify</Link></Typography>
-          
+          <Typography variant="h6" style={{ flexGrow: 1 }} color="inherit">
+            <Link className={classes.navLink} to="/">
+              Reactify
+            </Link>
+          </Typography>
+
           <Link className={classes.navLink} to="/products">
-            <Button variant = "text" className = {classes.padding} color="inherit" >Products</Button>
+            <Button variant="text" className={classes.padding} color="inherit">
+              Products
+            </Button>
           </Link>
-            {roleName === 'Admin' ? (
-              <Link className={classes.navLink} to="/dashboard">
-                <Button variant = "text" className = {classes.padding} color="inherit" >Admin Panel</Button>
-              </Link>
-            ): (
-              <Link className={classes.navLink} to="/myOrders">
-                <Button variant = "text" className = {classes.padding} color="inherit" >Dashboard</Button>
-              </Link>
-            )}
-          
-          <Button variant = "text" className = {classes.padding} color="inherit" onClick={logoutFunction}>Logout</Button>
+          {roleName === "Admin" ? (
+            <Link className={classes.navLink} to="/dashboard">
+              <Button
+                variant="text"
+                className={classes.padding}
+                color="inherit"
+              >
+                Admin Panel
+              </Button>
+            </Link>
+          ) : (
+            <Link className={classes.navLink} to="/myOrders">
+              <Button
+                variant="text"
+                className={classes.padding}
+                color="inherit"
+              >
+                Dashboard
+              </Button>
+            </Link>
+          )}
+
+          <Button
+            variant="text"
+            className={classes.padding}
+            color="inherit"
+            onClick={logoutFunction}
+          >
+            Logout
+          </Button>
 
           <CartButton cartLength={cartLength} />
         </Toolbar>
       </AppBar>
-    )
+    );
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
         {this.state.drawerActivate ? this.createDrawer() : this.destroyDrawer()}
       </div>
@@ -157,10 +214,8 @@ class AppBarAuth extends Component{
 }
 
 AppBarAuth.propTypes = {
-  classes : PropTypes.object.isRequired,
-  history : PropTypes.object
+  classes: PropTypes.object.isRequired,
+  history: PropTypes.object,
 };
-
-
 
 export default AppBarAuth;
